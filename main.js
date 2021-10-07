75d344c38ff6d5c95e16b74e3185ffdbf56242d4
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-10-07 16:43:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('pose is initialised');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY="+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftwristX="+leftWristX+"rightwristX="+rightWristX+"difference="+difference);
+    }
+}
+function draw(){
+    background('#969A97');
+    document.getElementById("square_side").innerHTML="width and height of a square will be="+difference+"px";
+    fill('#f90093');
+    stroke('#f90093');
+    square(noseX,noseY,difference);
+}
